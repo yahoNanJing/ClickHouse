fcc8d991060e73774b2803be166f1a047ce37d96
Yanghong Zhong <yangzhong@ebay.com>
2025-05-28 13:53:03 +0800

    Revert "[COLUMNAR-3600] Skip the iceberg metadata initialization for the worker nodes"
    This reverts commit 0c9795578a6b90f1f45b303ac09a1e7ead4cb5eb.

--- src/Storages/ObjectStorage/StorageObjectStorage.cpp
+++ src/Storages/ObjectStorage/StorageObjectStorage.cpp
@@ -105,11 +105,6 @@ StorageObjectStorage::StorageObjectStorage(
     bool failed_init = false;
     auto do_init = [&]()
     {
-        if (distributed_processing)
-        {
-            // No need to initialize configuration on distributed processing
-            return;
-        }
         try
         {
             if (configuration->hasExternalDynamicMetadata())
@@ -151,9 +146,7 @@ StorageObjectStorage::StorageObjectStorage(
     if (!failed_init
         && sample_path.empty()
         && context->getSettingsRef()[Setting::use_hive_partitioning]
-        && !configuration->withPartitionWildcard()
-        // No need to get sample path on distributed processing which is also not feasible due to no initialization of configuration
-        && !distributed_processing)
+        && !configuration->withPartitionWildcard())
     {
         if (do_lazy_init)
             do_init();
